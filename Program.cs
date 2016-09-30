fb04fade125a23dbe2d86329ae9feeda305fcb20
Joe Segars <joemsegars@gmail.com>
2016-09-30 13:19:57 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AttCommunicationsManager.Classes;
+using System.Net;
+
+namespace SimpleWebServer
+{
+    class Program
+    {
+        private static WebServer _webServer;
+
+        static void Main(string[] args)
+        {
+            StartWebServer();
+        }
+
+        private static void StartWebServer()
+        {
+            if (_webServer == null)
+            {
+                _webServer = new WebServer(SendResponse, "http://localhost:2468/auth/callback/");
+            }
+
+            _webServer.Run();
+
+            Console.ReadKey();
+        }
+
+        private static void StopWebServer()
+        {
+            _webServer.Stop();
+        }
+
+        private static String SendResponse(HttpListenerRequest request)
+        {
+            return "<HTML><BODY>derp</BODY></HTML>";
+        }
+    }
+}
